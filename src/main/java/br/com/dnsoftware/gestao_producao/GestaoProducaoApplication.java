757747ef7ff3a3f9f2910c5f8713f4d76f66620c
Denis Gabriel <denisgabriel017@gmail.com>
2025-09-03 23:07:51 -0300

    Criada a parte logica da aplicação

---
+++ src/main/java/br/com/dnsoftware/gestao_producao/GestaoProducaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dnsoftware.gestao_producao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProducaoApplication.class, args);
+	}
+
+}
